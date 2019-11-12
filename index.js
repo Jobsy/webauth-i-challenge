90cb5512e7b50f5a2ea5a20237782abe1b342069
Oluwajoba <oluwajobabello@gmail.com>
2019-11-12 08:08:22 +0100

    register new user with bcrypt / api/register / index.js

--- index.js
+++ index.js
@@ -13,49 +13,49 @@ server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
-  res.send("It's alive!");
+    res.send("It's alive!");
 });
 
 server.post('/api/register', (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(users.password, 11)
-  const user1 = {
-username: req.body.username,
-password: hash,
-  }
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 11)
+    const user1 = {
+        username: req.body.username,
+        password: hash,
+    }
 
-  Users.add(user1)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    Users.add(user1)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(":::"+error);
+        });
 });
 
 server.post('/api/login', (req, res) => {
-  let { username, password } = req.body;
+    let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({ message: `Welcome ${user.username}!` });
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
 server.get('/api/users', (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
 });
 
 const port = process.env.PORT || 5000;
