082047e4ccfd37573c672cdeb3d26db854e17ca2
Oluwajoba <oluwajobabello@gmail.com>
2019-11-12 18:23:31 +0100

    update restricted() / index.js

--- index.js
+++ index.js
@@ -51,21 +51,30 @@ server.post('/api/login', (req, res) => {
     });
 });
 
+// function restricted(req, res, next) {
+//   const { username, password } = req.headers
+//   Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//       if (user && bcrypt.compareSync(password, user.password)) {
+//         next()
+//       } else {
+//         res.status(401).json({ message: 'Invalid Credentials' });
+//       }
+//     })
+//     .catch(error => {
+//       res.status(500).json({ message: error.message });
+//     });
+// }
+
 function restricted(req, res, next) {
-  const { username, password } = req.headers
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        next()
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ message: error.message });
-    });
-}
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
+};
+
 
 server.get('/api/users', restricted, (req, res) => {
   Users.find()
