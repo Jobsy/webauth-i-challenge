92238d7a740ea66c77a9277d33fc2d6daaaff96b
Oluwajoba <oluwajobabello@gmail.com>
2019-11-12 08:27:37 +0100

    add middleware / restricted() / index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server.post('/api/register', (req, res) => {
             res.status(201).json(saved);
         })
         .catch(error => {
-            res.status(500).json("error while loging in: "+error);
+            res.status(500).json("error while loging in: " + error);
         });
 });
 
@@ -46,11 +46,26 @@ server.post('/api/login', (req, res) => {
             }
         })
         .catch(error => {
-            res.status(500).json(error);
+            res.status(500).json(message: error.message);
         });
 });
 
-server.get('/api/users', (req, res) => {
+function restricted(req, res, next) {
+    const { username, password } = req.headers
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                next()
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+}
+server.get('/api/users', restricted, (req, res) => {
     Users.find()
         .then(users => {
             res.json(users);
