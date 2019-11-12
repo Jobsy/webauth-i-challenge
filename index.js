e8b82197a29c0e2ba7de3d67ed0b829e5075fc06
Oluwajoba <oluwajobabello@gmail.com>
2019-11-12 18:14:37 +0100

    update post(/api/login) to use session / index.js

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
