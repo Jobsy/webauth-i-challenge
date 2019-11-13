5ea2d4d439cef4a77b6c371e17617e4e256f3219
Oluwajoba <oluwajobabello@gmail.com>
2019-11-13 02:01:44 +0100

    add sessionConfig file / index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const KnexSessionStore = require('connect-session-knex')(session);
 
@@ -10,6 +10,25 @@ const db = require('./data/dbConfig.js');
 const Users = require('./users/users-model.js');
 
 const server = express();
+
+const sessionConfig = {
+  name: 'myFirstCookieSession',
+  secret: 'make it a little long and keep it safe!',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, 
+    httpOnly: false,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: require('../database/dbConfig'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+}
 
 server.use(helmet());
 server.use(express.json());
